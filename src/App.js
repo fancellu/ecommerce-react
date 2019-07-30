1c8abdc547325fff5bedde9910808778a5598756
Dino Fancellu <dino@felstar.com>
2019-07-31 00:50:23 +0100

    simple homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component'
 
 function App() {
   return (
     <div>
-      Hello World
+        <HomePage/>
     </div>
   );
 }
