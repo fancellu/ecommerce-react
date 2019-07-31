37fc3e7c02d574eadf46d1943e69ac46dcceabb1
Dino Fancellu <dino@felstar.com>
2019-07-31 14:31:46 +0100

    Breaking into components, images etc

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './homepage.styles.scss'
+
+import Directory from '../../components/directory/directory.component'
+
+const HomePage= () => (
+    <div className="homepage">
+        <Directory/>
+    </div>
+)
+
+export default HomePage
