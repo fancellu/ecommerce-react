f742eea33838f69fc247705898eae721c24dc779
Dino Fancellu <dino@felstar.com>
2019-08-01 13:48:03 +0100

    Added header and logo

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 
 function App() {
   return (
     <div>
+        <Header/>
         <Switch>
             <Route path='/shop' component={ShopPage}/>
             <Route path='/' component={HomePage}/>
