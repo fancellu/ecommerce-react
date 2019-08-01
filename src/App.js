97b4bd89dd840a5b1eb40c16749cd0ef9777de7a
Dino Fancellu <dino@felstar.com>
2019-08-01 15:03:51 +0100

    Adding sign-in

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
-
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
         <Header/>
         <Switch>
+            <Route path='/signin' component={SignInAndSignUpPage}/>
             <Route path='/shop' component={ShopPage}/>
             <Route path='/' component={HomePage}/>
         </Switch>
