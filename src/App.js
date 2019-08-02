42a13ce73e0d1ceafd4971cb30ac101b89cb3955
Dino Fancellu <dino@felstar.com>
2019-08-02 17:23:22 +0100

    Now stores user profile in firebase db

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth} from './firebase/firebase.utils'
-
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
     constructor() {
@@ -20,9 +19,8 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user})
-            console.log(user)
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+            createUserProfileDocument(user)
         })
     }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,9 +2,9 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-const config={
+const config = {
     apiKey: "AIzaSyBnnJtoHyoOdi_YmJfU20FpNqSgCIik2BM",
-        authDomain: "react-shop-db-63a4b.firebaseapp.com",
+    authDomain: "react-shop-db-63a4b.firebaseapp.com",
     databaseURL: "https://react-shop-db-63a4b.firebaseio.com",
     projectId: "react-shop-db-63a4b",
     storageBucket: "",
@@ -12,12 +12,36 @@ const config={
     appId: "1:183706946465:web:58eb2a86b4412820"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`/users/${userAuth.uid}`)
+
+    const snapshot = await userRef.get()
+
+    console.log(snapshot)
+    if (!snapshot.exists) {
+        const {displayName, email} = userAuth
+        const createAt = new Date()
+        try {
+         console.log('saving new user')
+         await userRef.set({
+             displayName, email, createAt, ...additionalData
+         })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+
+    return userRef
+}
+
 firebase.initializeApp(config)
 
-export const auth=firebase.auth()
-export const firestore=firebase.firestore()
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
 
-const provider=new firebase.auth.GoogleAuthProvider()
+const provider = new firebase.auth.GoogleAuthProvider()
 provider.setCustomParameters({
     prompt: 'select_account'
 })
