5148aeb164972d98a21d63a2c0823fe99b4d09ad
Dino Fancellu <dino@felstar.com>
2019-08-02 15:24:02 +0100

    Getting the current auth from firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import {auth} from './firebase/firebase.utils'
 
-function App() {
-  return (
-    <div>
-        <Header/>
-        <Switch>
-            <Route path='/signin' component={SignInAndSignUpPage}/>
-            <Route path='/shop' component={ShopPage}/>
-            <Route path='/' component={HomePage}/>
-        </Switch>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+            this.setState({currentUser: user})
+            console.log(user)
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth()
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Switch>
+                    <Route path='/signin' component={SignInAndSignUpPage}/>
+                    <Route path='/shop' component={ShopPage}/>
+                    <Route path='/' component={HomePage}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
